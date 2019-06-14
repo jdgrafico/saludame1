f9ba8eedd70737a432793dc843ba2a6eaa6be39b
jdgrafico <juandagomezm@gmail.com>
2019-06-13 20:28:28 -0500

    mejorando el archivo de sinatra anterior

--- solution.rb
+++ solution.rb
@@ -1,10 +1,8 @@
 require 'sinatra'
 
-get '/' do
-    if params[:nombre] == "Juan"
+get '/makers/:nombre' do
+    
          "<h1> Hola #{params[:nombre]}! </h1> "
-    else 
-        "<h1> Hola desconocido!</1>" 
-                  end
+   
     
    end
